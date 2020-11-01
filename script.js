a09ba4e08aaa5b9309419c503dba4a95f00ea319
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-11-01 10:49:20 -0500

    added function to add proper styling based on time of day

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
-let workDay = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM" : "",
-    "5 PM": "",
-};
+// let workDay = {
+//     "8 AM": "",
+//     "9 AM": "",
+//     "10 AM": "",
+//     "11 AM": "",
+//     "12 PM": "",
+//     "1 PM": "",
+//     "2 PM": "",
+//     "3 PM": "",
+//     "4 PM" : "",
+//     "5 PM": "",
+// };
 
 //set current date and time to top of page
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
